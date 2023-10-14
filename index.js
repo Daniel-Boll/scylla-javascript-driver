c2fe7c927f59f0b943e51557645c38c62d61e66a
Daniel Boll <danielboll.academico@gmail.com>
2023-10-13 21:50:51 -0300

    chore(Cargo.toml): remove unused feature flag for scylla crate chore(index.js): update require statements for scylla native bindings to match new package names and file names

--- index.js
+++ index.js
@@ -32,24 +32,24 @@ switch (platform) {
   case 'android':
     switch (arch) {
       case 'arm64':
-        localFileExisted = existsSync(join(__dirname, 'scylladb-driver.android-arm64.node'))
+        localFileExisted = existsSync(join(__dirname, 'scylladb.android-arm64.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./scylladb-driver.android-arm64.node')
+            nativeBinding = require('./scylladb.android-arm64.node')
           } else {
-            nativeBinding = require('scylladb-driver-android-arm64')
+            nativeBinding = require('@lambda-group/scylladb-android-arm64')
           }
         } catch (e) {
           loadError = e
         }
         break
       case 'arm':
-        localFileExisted = existsSync(join(__dirname, 'scylladb-driver.android-arm-eabi.node'))
+        localFileExisted = existsSync(join(__dirname, 'scylladb.android-arm-eabi.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./scylladb-driver.android-arm-eabi.node')
+            nativeBinding = require('./scylladb.android-arm-eabi.node')
           } else {
-            nativeBinding = require('scylladb-driver-android-arm-eabi')
+            nativeBinding = require('@lambda-group/scylladb-android-arm-eabi')
           }
         } catch (e) {
           loadError = e
@@ -63,13 +63,13 @@ switch (platform) {
     switch (arch) {
       case 'x64':
         localFileExisted = existsSync(
-          join(__dirname, 'scylladb-driver.win32-x64-msvc.node')
+          join(__dirname, 'scylladb.win32-x64-msvc.node')
         )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./scylladb-driver.win32-x64-msvc.node')
+            nativeBinding = require('./scylladb.win32-x64-msvc.node')
           } else {
-            nativeBinding = require('scylladb-driver-win32-x64-msvc')
+            nativeBinding = require('@lambda-group/scylladb-win32-x64-msvc')
           }
         } catch (e) {
           loadError = e
@@ -77,13 +77,13 @@ switch (platform) {
         break
       case 'ia32':
         localFileExisted = existsSync(
-          join(__dirname, 'scylladb-driver.win32-ia32-msvc.node')
+          join(__dirname, 'scylladb.win32-ia32-msvc.node')
         )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./scylladb-driver.win32-ia32-msvc.node')
+            nativeBinding = require('./scylladb.win32-ia32-msvc.node')
           } else {
-            nativeBinding = require('scylladb-driver-win32-ia32-msvc')
+            nativeBinding = require('@lambda-group/scylladb-win32-ia32-msvc')
           }
         } catch (e) {
           loadError = e
@@ -91,13 +91,13 @@ switch (platform) {
         break
       case 'arm64':
         localFileExisted = existsSync(
-          join(__dirname, 'scylladb-driver.win32-arm64-msvc.node')
+          join(__dirname, 'scylladb.win32-arm64-msvc.node')
         )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./scylladb-driver.win32-arm64-msvc.node')
+            nativeBinding = require('./scylladb.win32-arm64-msvc.node')
           } else {
-            nativeBinding = require('scylladb-driver-win32-arm64-msvc')
+            nativeBinding = require('@lambda-group/scylladb-win32-arm64-msvc')
           }
         } catch (e) {
           loadError = e
@@ -108,23 +108,23 @@ switch (platform) {
     }
     break
   case 'darwin':
-    localFileExisted = existsSync(join(__dirname, 'scylladb-driver.darwin-universal.node'))
+    localFileExisted = existsSync(join(__dirname, 'scylladb.darwin-universal.node'))
     try {
       if (localFileExisted) {
-        nativeBinding = require('./scylladb-driver.darwin-universal.node')
+        nativeBinding = require('./scylladb.darwin-universal.node')
       } else {
-        nativeBinding = require('scylladb-driver-darwin-universal')
+        nativeBinding = require('@lambda-group/scylladb-darwin-universal')
       }
       break
     } catch {}
     switch (arch) {
       case 'x64':
-        localFileExisted = existsSync(join(__dirname, 'scylladb-driver.darwin-x64.node'))
+        localFileExisted = existsSync(join(__dirname, 'scylladb.darwin-x64.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./scylladb-driver.darwin-x64.node')
+            nativeBinding = require('./scylladb.darwin-x64.node')
           } else {
-            nativeBinding = require('scylladb-driver-darwin-x64')
+            nativeBinding = require('@lambda-group/scylladb-darwin-x64')
           }
         } catch (e) {
           loadError = e
@@ -132,13 +132,13 @@ switch (platform) {
         break
       case 'arm64':
         localFileExisted = existsSync(
-          join(__dirname, 'scylladb-driver.darwin-arm64.node')
+          join(__dirname, 'scylladb.darwin-arm64.node')
         )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./scylladb-driver.darwin-arm64.node')
+            nativeBinding = require('./scylladb.darwin-arm64.node')
           } else {
-            nativeBinding = require('scylladb-driver-darwin-arm64')
+            nativeBinding = require('@lambda-group/scylladb-darwin-arm64')
           }
         } catch (e) {
           loadError = e
@@ -152,12 +152,12 @@ switch (platform) {
     if (arch !== 'x64') {
       throw new Error(`Unsupported architecture on FreeBSD: ${arch}`)
     }
-    localFileExisted = existsSync(join(__dirname, 'scylladb-driver.freebsd-x64.node'))
+    localFileExisted = existsSync(join(__dirname, 'scylladb.freebsd-x64.node'))
     try {
       if (localFileExisted) {
-        nativeBinding = require('./scylladb-driver.freebsd-x64.node')
+        nativeBinding = require('./scylladb.freebsd-x64.node')
       } else {
-        nativeBinding = require('scylladb-driver-freebsd-x64')
+        nativeBinding = require('@lambda-group/scylladb-freebsd-x64')
       }
     } catch (e) {
       loadError = e
@@ -168,26 +168,26 @@ switch (platform) {
       case 'x64':
         if (isMusl()) {
           localFileExisted = existsSync(
-            join(__dirname, 'scylladb-driver.linux-x64-musl.node')
+            join(__dirname, 'scylladb.linux-x64-musl.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./scylladb-driver.linux-x64-musl.node')
+              nativeBinding = require('./scylladb.linux-x64-musl.node')
             } else {
-              nativeBinding = require('scylladb-driver-linux-x64-musl')
+              nativeBinding = require('@lambda-group/scylladb-linux-x64-musl')
             }
           } catch (e) {
             loadError = e
           }
         } else {
           localFileExisted = existsSync(
-            join(__dirname, 'scylladb-driver.linux-x64-gnu.node')
+            join(__dirname, 'scylladb.linux-x64-gnu.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./scylladb-driver.linux-x64-gnu.node')
+              nativeBinding = require('./scylladb.linux-x64-gnu.node')
             } else {
-              nativeBinding = require('scylladb-driver-linux-x64-gnu')
+              nativeBinding = require('@lambda-group/scylladb-linux-x64-gnu')
             }
           } catch (e) {
             loadError = e
@@ -197,26 +197,26 @@ switch (platform) {
       case 'arm64':
         if (isMusl()) {
           localFileExisted = existsSync(
-            join(__dirname, 'scylladb-driver.linux-arm64-musl.node')
+            join(__dirname, 'scylladb.linux-arm64-musl.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./scylladb-driver.linux-arm64-musl.node')
+              nativeBinding = require('./scylladb.linux-arm64-musl.node')
             } else {
-              nativeBinding = require('scylladb-driver-linux-arm64-musl')
+              nativeBinding = require('@lambda-group/scylladb-linux-arm64-musl')
             }
           } catch (e) {
             loadError = e
           }
         } else {
           localFileExisted = existsSync(
-            join(__dirname, 'scylladb-driver.linux-arm64-gnu.node')
+            join(__dirname, 'scylladb.linux-arm64-gnu.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./scylladb-driver.linux-arm64-gnu.node')
+              nativeBinding = require('./scylladb.linux-arm64-gnu.node')
             } else {
-              nativeBinding = require('scylladb-driver-linux-arm64-gnu')
+              nativeBinding = require('@lambda-group/scylladb-linux-arm64-gnu')
             }
           } catch (e) {
             loadError = e
@@ -225,13 +225,13 @@ switch (platform) {
         break
       case 'arm':
         localFileExisted = existsSync(
-          join(__dirname, 'scylladb-driver.linux-arm-gnueabihf.node')
+          join(__dirname, 'scylladb.linux-arm-gnueabihf.node')
         )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./scylladb-driver.linux-arm-gnueabihf.node')
+            nativeBinding = require('./scylladb.linux-arm-gnueabihf.node')
           } else {
-            nativeBinding = require('scylladb-driver-linux-arm-gnueabihf')
+            nativeBinding = require('@lambda-group/scylladb-linux-arm-gnueabihf')
           }
         } catch (e) {
           loadError = e
