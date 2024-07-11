1a08d2096f8cdd3fcfe43e97bcf4b7f0251de682
Felipi Lima Matozinho <50120388+Matozinho@users.noreply.github.com>
2024-07-11 18:39:41 -0300

    feat(cluster): add keyspace, auth, and ssl fields to ClusterConfig struct to (#25)
    improve configuration flexibility and support for authentication and SSL
    connections
    
    Signed-off-by: matozinho <matozinhof.academico@gmail.com>

--- src/cluster/cluster_config/mod.rs
+++ src/cluster/cluster_config/mod.rs
@@ -1,5 +1,7 @@
 use crate::cluster::{
-  cluster_config::compression::Compression, execution_profile::ExecutionProfile,
+  cluster_config::compression::Compression,
+  execution_profile::ExecutionProfile,
+  scylla_cluster::{Auth, Ssl},
 };
 
 pub mod compression;
@@ -9,4 +11,9 @@ pub struct ClusterConfig {
   pub nodes: Vec<String>,
   pub compression: Option<Compression>,
   pub default_execution_profile: Option<ExecutionProfile>,
+
+  // connection fields
+  pub keyspace: Option<String>,
+  pub auth: Option<Auth>,
+  pub ssl: Option<Ssl>,
 }
